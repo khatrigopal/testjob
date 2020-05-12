94e1768be3dea534d7e20a76ab24342e05249dff
Gopal Khatri <khatri.gopal@gmail.com>
2020-05-12 14:49:33 +0000

    confirm

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <?php
 print "Welcome My Amazing Cloud World!";
 echo "<br>";
-print "This is my India"
+print "This is my India";
 echo "<br>";
+print "Hi";
 echo "<br>";
 echo "<br>";
 print `ifconfig`;
